d94010c96bd2ec1c646d893c144abeedb89c4850
Advait Jain <aj.advaitj@gmail.com>
2022-10-21 21:52:22 +0530

    This is a test commit

---
+++ List of Even Numbers.py
@@ -0,0 +1,8 @@
+# Program: List of Even Numbers
+
+# Create a list of even numbers from 0 to 10
+even_numbers = [x for x in range(0, 11) if x % 2 == 0]
+
+# Print the list of even numbers
+print("List of even numbers from 0 to 10:")
+print(even_numbers)
